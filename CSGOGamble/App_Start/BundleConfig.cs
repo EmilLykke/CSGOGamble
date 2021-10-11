d90babad8c33e76ff51d0a124c4bad34eea42916
EmilLykke <emillykkeg@gmail.com>
2021-10-11 13:51:57 +0200

    Svg's

--- CSGOGamble/App_Start/BundleConfig.cs
+++ CSGOGamble/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace CSGOGamble
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Navbar.css"));
         }
     }
 }
