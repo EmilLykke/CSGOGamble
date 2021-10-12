d2d91d77aa037c234100c59e9629b4f1aab42b12
aske2002 <askevkoed@gmail.com>
2021-10-12 11:58:58 +0200

    XD

---
+++ CSGOGamble/BettingHub.cs
@@ -0,0 +1,18 @@
+﻿using CSGOGamble.Models;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSGOGamble
+{
+    public class BettingHub : Hub
+    {
+        public void SendAllBets(List<betModel> bets)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(bets);
+        }
+    }
+}
---
+++ CSGOGamble/Models/betModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSGOGamble.Models
+{
+    public class betModel
+    {
+
+        public int roundId { get; set; }
+        public float totalAmount { get; set; }
+        public List<bet> bets { get; set; }
+        public betModel (int roundId, float totalAmount, List<bet> bets)
+        {
+            this.bets = bets;
+            this.totalAmount = totalAmount;
+            this.roundId = roundId;
+        }
+    }
+
+    public class bet
+    {
+        public string user_name { get; set; }
+        public float amount { get; set; }
+        public string color { get; set; }
+        public bet(string user_name, float amount, string color)
+        {
+            this.amount = amount;
+            this.user_name = user_name;
+            this.color = color;
+        }
+    }
+}
