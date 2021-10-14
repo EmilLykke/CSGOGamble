ba9fb2d418dd733452e4508397d0fca256aabf7b
Aske <Askevkoed@gmail.com>
2021-10-14 23:17:07 +0200

    Trying to implement roll every 40 second, apparently code stops thread. Fix in Startup.cs

--- CSGOGamble/BettingHub.cs
+++ CSGOGamble/BettingHub.cs
@@ -2,26 +2,15 @@
 using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace CSGOGamble
 {
     public class BettingHub : Hub
     {
-        private CsgoBettingEntities1 databaseManager = new CsgoBettingEntities1();
-        private void RunBet()
-        {
-            int keyId = databaseManager.roundkeys.Max(x => x.ID);
-            roundkey key = databaseManager.roundkeys.FirstOrDefault(x => x.ID == keyId);
-            if(key != null)
-            {
-                if (key.date == DateTime.UtcNow.Date)
-                {
-
-                }
-            }
-        }
         public void SendAllBets(List<betModel> bets)
         {
             // Call the broadcastMessage method to update clients.
