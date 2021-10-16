965d3631b86e145e5dbe1094d26d5f02e9fab130
Aske <Askevkoed@gmail.com>
2021-10-16 14:42:39 +0200

    Making bets

---
+++ CSGOGamble/Models/BetResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSGOGamble.Models
+{
+    public class BetResult
+    {
+        public bool error = false;
+        public double newAmount { get; set; }
+        public BetResult(double newAmount)
+        {
+            this.newAmount = newAmount;
+        } 
+    }
+}
---
+++ CSGOGamble/Models/PostError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSGOGamble.Models
+{
+    public class PostError
+    {
+        public bool error = true;
+        public string tile { get; set; }
+        public string message { get; set; }
+        public PostError(string title, string message)
+        {
+            this.tile = title;
+            this.message = message;
+        }
+
+    }
+}
