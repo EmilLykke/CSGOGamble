1d2a3aa18c6dcccce941f8f83c57c73585e2a883
aske2002 <askevkoed@gmail.com>
2021-10-25 11:53:34 +0200

    Nice

--- CSGOGamble/Controllers/HomeController.cs
+++ CSGOGamble/Controllers/HomeController.cs
@@ -7,19 +7,28 @@ using System.Web.Mvc;
 
 namespace CSGOGamble.Controllers
 {
+    //Dette er vores homecontroller hvor vi håndtere requests til selve hjemmesiden, der er kun en side - Index - Så det er ret simpelt.
     public class HomeController : Controller
     {
+        //Vi opretter et nyt entity af vores database
         private CsgoBettingEntities1 databaseManager = new CsgoBettingEntities1();
 
+        //Her er vores actionresult når folk går ind på /Home/Index
         public ActionResult Index()
         {
+            //I de første par linjer finder vi blot de informationer brugeren skal bruge når han/hun kommer ind på hjemmesiden, det er f.eks. hvor mange penge brugeren har, hvad de udkommet af de
+            //sidste 10 runder er, udkommet af de sidste 100 runder, hvilke bets der er i den nuværende runde og mere. Al denne information bliver sat ind i en IndexModel som bliver sendt videre med View()
+            //Find den nyeste runde
             rounds round = databaseManager.rounds.SingleOrDefault(r => r.ID == databaseManager.rounds.Max(z => z.ID));
-            List<bets> bets = databaseManager.bets.Where(x => x.roundID == round.ID).ToList();
+            //Find bets for runden
+            List<bets> bets = round.bets.ToList();
+            //Find de sidste 100 bets
             List<rounds> last100 = this.databaseManager.rounds.Where(x => x.complete == 1).OrderByDescending(x => x.ID).Take(100).ToList();
             List<rounds> last10 = last100.OrderByDescending(x => x.ID).Take(10).OrderBy(x => x.ID).ToList();
             int counter = 0;
             int terrorist = 0;
             int jackpot = 0;
+            //Udregn procentdel for hhv. counter, terrorist og jackpot for hvad de sidste 100 runder er blevet.
             foreach (var round100 in last100)
             {
                 if (round100.color == "counter")
@@ -35,17 +44,22 @@ namespace CSGOGamble.Controllers
                     jackpot++;
                 }
             }
+            //Lav en indexmode som tager et brugernavn, mængde af benge, sidste 10 bets, og procentdel af udkom.
             IndexModel model = new IndexModel(null, double.NaN, last10, bets, counter, terrorist, jackpot);
+            //Tjek om brugeren er logget ind
             if (Request.IsAuthenticated)
             {
                 string id = User.Identity.Name;
                 int idint = Int32.Parse(id);
                 users user = this.databaseManager.users.SingleOrDefault(u => u.ID == idint);
+                //Ændrer værdierne i indexmodel for username og amount
                 model.Username = user.username;
                 model.Amount = user.amount;
+                //Retuner index.cshtml view
                 return View(model);
             } else
             {
+                //Retuner index.cshtml view hvor username og amount er null da brugeren ikke er logget ind
                 return View(model);
             }
         }
